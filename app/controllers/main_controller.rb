3ff04bbd860aea1dd0d52b6d684a681cee50be1e
Justice Otuya <jotuya2@gmail.com>
2023-10-29 22:40:43 +0100

    add flash messages

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,4 +1,7 @@
 class MainController < ApplicationController
   def index
+    flash.now[:notice] = 'Login Sucessfully'
+    flash.now[:alert] = 'I am warning you'
+    flash.now[:error] = 'this is an error alert'
   end
 end
